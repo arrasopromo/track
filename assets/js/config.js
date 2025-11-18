2e2129e9dd6be38bd06e9bee73d418ec05f9b667
arrasopromo <arrasopromo@users.noreply.github.com>
2025-11-17 23:45:03 -0300

    track34

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 window.TRACK_CONFIG = {
   webhookUrl: '/api/track',
   defaultWhatsAppPhone: '5522981772788',
-  defaultMessage: 'abc',
+  defaultMessage: 'Tenho interesse em saber mais sobre os serviços |',
   appendUtmToMessage: false,
   autoRedirectOnLoad: true,
   pixelId: '1019661457030791'
